d06ddaf26f3f461e614490101cf512e8b6554e1d
yspcn <55094985+yspcn@users.noreply.github.com>
2022-08-14 09:59:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,5 +24,6 @@
 
 # Ôö¼Óssr
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small package/small
 # git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
 git clone https://github.com/yufan666/luci-app-npc package/openwrt-packages/luci-app-npc
