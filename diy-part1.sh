3e1fb896c41bb32e0a18401b58d75fdc8a6c168a
eysp <60641231+eysp@users.noreply.github.com>
2022-08-18 08:58:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,7 +25,6 @@
 # Ôö¼Óssr
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small package/small
-git clone https://github.com/kiddin9/openwrt-packages package/ysp
 git clone https://github.com/ghosthgytop/npc package/npc
 svn co https://github.com/immortalwrt/packages/trunk/lang/lua-neturl package/lang/lua-neturl
 # git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
@@ -35,3 +34,15 @@ rm -rf package/emortal/default-settings
 svn co https://github.com/Lienol/openwrt/branches/22.03/package/default-settings package/default-settings
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree package/lean/luci-app-ramfree
 git clone https://github.com/Lienol/openwrt-OpenAppFilter package/openwrt-OpenAppFilter
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-autoreboot package/lean/luci-app-autoreboot
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-cpufreq package/lean/luci-app-cpufreq
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-guest-wifi package/lean/luci-app-guest-wifi
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-homeassistant package/lean/luci-app-homeassistant
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mwan3helper package/lean/luci-app-mwan3helper
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/lean/luci-app-turboacc
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/lean/luci-app-vlmcsd
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-zerotier package/lean/luci-app-zerotier
+svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/lean/vlmcsd
+svn co https://github.com/kiddin9/openwrt-packages/trunk/dnsforwarder package/lean/dnsforwarder
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-lib-iform package/lean/luci-lib-iform
+svn co https://github.com/kiddin9/openwrt-packages/trunk/pdnsd-alt package/lean/pdnsd-alt
