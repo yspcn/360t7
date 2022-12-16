b54c0727fcf5d871788003639797c556a5966dbf
yspcn <55094985+yspcn@users.noreply.github.com>
2022-12-16 10:18:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,4 +28,4 @@ git clone https://github.com/kenzok8/small package/small
 svn co https://github.com/immortalwrt/packages/trunk/lang/lua-neturl package/lang/lua-neturl
 # git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
 svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-npc package/luci-app-npc
-git clone https://github.com/khongpt/luci-app-cloudflared package/lean/luci-app-cloudflared
+#git clone https://github.com/khongpt/luci-app-cloudflared package/lean/luci-app-cloudflared
