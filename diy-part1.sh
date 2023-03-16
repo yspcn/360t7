f77aa74f20d9e001136a36adc4bd00605dd1ca31
yspcn <55094985+yspcn@users.noreply.github.com>
2023-03-16 16:32:12 +0800

    修复hysteria编译失败

--- diy-part1.sh
+++ diy-part1.sh
@@ -32,3 +32,4 @@ svn co https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-npc package
 sed -i 's/2023.03.22/2023.02.28/g' package/small/chinadns-ng/Makefile
 sed -i 's/1.8.0/1.7.5/g' package/small/xray-core/Makefile
 sed -i 's/35339ee4c2ebd46cebd95ad7791d38a97db3a6afd97277a33e4ee7be35c9a3b2/a5fc936136a57a463bf9a895d068fdfa895b168ae6093c58a10208e098b6b2d3/g' package/small/xray-core/Makefile
+sed -i 's/1.3.4/1.3.3/g' package/small/hysteria/Makefile
