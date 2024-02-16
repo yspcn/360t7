ef6b9c4e1c4207dff41a366ffe6dd2b58a7782a4
yspcn <55094985+yspcn@users.noreply.github.com>
2024-02-16 11:41:36 +0800

    Update 360t7-diy2.sh

--- 360t7-diy2.sh
+++ 360t7-diy2.sh
@@ -46,8 +46,8 @@ git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design
 #svn co https://github.com/openwrt/packages/branches/openwrt-19.07/net/wget feeds/packages/net/wget
 rm -rf feeds/packages/lang/golang
 #svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
-git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/packages
-cp -rf packages/lang/golang feeds/packages/lang/  && rm -rf packages
+git clone --depth=1 -b openwrt-23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
+cp -rf packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
