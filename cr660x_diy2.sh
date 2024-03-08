e34c38850645233ef7c3e2f85d08584e7cca2207
yspcn <55094985+yspcn@users.noreply.github.com>
2024-03-08 13:35:07 +0800

    Update cr660x_diy2.sh

--- cr660x_diy2.sh
+++ cr660x_diy2.sh
@@ -47,6 +47,14 @@ rm -rf feeds/packages/lang/golang
 git clone --depth=1 -b 23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
 cp -rf openwrt-packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
 sed -i 's/ +libopenssl-legacy//g' package/small/shadowsocksr-libev/Makefile
+# tailscale install
+mkdir -p package/utils/ucode
+wget -P package/utils/ucode/ https://github.com/openwrt/openwrt/raw/openwrt-22.03/package/utils/ucode/Makefile
+git clone --depth=1 https://github.com/openwrt/packages packages-temp
+rm -rf feeds/packages/net/tailscale && cp -rf packages-temp/net/tailscale feeds/packages/net/
+rm -rf packages-temp
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+rm -rf package/feeds/luci/luci-app-ipsec-vpnd package/feeds/luci/luci-app-ipsec-vpnserver-manyusers package/openwrt-packages/homeproxy
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
