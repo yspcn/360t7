d2d10351678c228865fd127c69dfc00ea44e8d7e
yspcn <55094985+yspcn@users.noreply.github.com>
2024-03-08 13:24:30 +0800

    Update rax3000m.sh

--- rax3000m.sh
+++ rax3000m.sh
@@ -3,6 +3,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
+# Modify hostname
+sed -i 's/ImmortalWrt/CMCC-rax3000m/g' package/base-files/files/bin/config_generate
+
 # Modify default golang
 rm -rf feeds/packages/lang/golang
 #svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
