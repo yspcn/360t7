fb31547bf25d3583f96f67f09353a5d491c51e8b
yspcn <55094985+yspcn@users.noreply.github.com>
2024-11-17 11:53:06 +0800

    Update 360t7-diy2.sh

--- 360t7-diy2.sh
+++ 360t7-diy2.sh
@@ -47,9 +47,10 @@ git clone https://github.com/gngpp/luci-theme-design package/luci-theme-design
 #rm -rf feeds/packages/net/wget
 #svn co https://github.com/openwrt/packages/branches/openwrt-19.07/net/wget feeds/packages/net/wget
 rm -rf feeds/packages/lang/golang
-#svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
-git clone --depth=1 -b 23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
-cp -rf openwrt-packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
+
+#git clone --depth=1 -b 23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
+#cp -rf openwrt-packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
+
 sed -i 's/ +libopenssl-legacy//g' package/small/shadowsocksr-libev/Makefile
 # tailscale install
 mkdir -p package/utils/ucode
