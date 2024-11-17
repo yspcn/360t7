9fcee9a81334e6920bfb4a5a8baf1450f4e9dda6
yspcn <55094985+yspcn@users.noreply.github.com>
2024-11-17 10:16:23 +0800

    Update cr660x_diy2.sh

--- cr660x_diy2.sh
+++ cr660x_diy2.sh
@@ -14,6 +14,13 @@
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
+# Modify default golang
+rm -rf feeds/packages/lang/golang
+#svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
+git clone --depth=1 -b 23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
+cp -rf openwrt-packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
+sed -i 's/ +libopenssl-legacy//g' package/small/shadowsocksr-libev/Makefile
+
 # Modify hostname
 sed -i 's/ImmortalWrt/CR660x/g' package/base-files/files/bin/config_generate
 
