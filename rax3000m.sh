e05f17267486a6652f7ef10479ff14c0dce49793
yspcn <55094985+yspcn@users.noreply.github.com>
2025-05-06 21:09:51 +0800

    Update rax3000m.sh

--- rax3000m.sh
+++ rax3000m.sh
@@ -11,7 +11,7 @@ rm -rf feeds/packages/lang/golang
 #svn co https://github.com/immortalwrt/packages/branches/openwrt-23.05/lang/golang feeds/packages/lang/golang
 git clone --depth=1 -b 23.05 https://github.com/Lienol/openwrt-packages #https://github.com/immortalwrt/packages
 cp -rf openwrt-packages/lang/golang feeds/packages/lang/  && rm -rf openwrt-packages
-sed -i 's/ +libopenssl-legacy//g' package/small/shadowsocksr-libev/Makefile
+#sed -i 's/ +libopenssl-legacy//g' package/small/shadowsocksr-libev/Makefile
 
 # tailscale install
 mkdir -p package/utils/ucode
